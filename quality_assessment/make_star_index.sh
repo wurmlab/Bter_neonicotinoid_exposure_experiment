7d13d9e0f85548fec45c6f0dedb3d8ebde4199d9
Joe Colgan <t.colgan@qmul.ac.uk>
2018-05-03 16:52:12 +0100

    Correct typo in usage error print out

--- quality_assessment/make_star_index.sh
+++ quality_assessment/make_star_index.sh
@@ -8,7 +8,7 @@ overhang=$3
 ## Check arguments are provided:
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied. Usage: ./make_STAR.sh input.fasta input.gtf overhang"
+    echo "No arguments supplied. Usage: ./make_star_index.sh input.fasta input.gtf overhang"
     echo "For overhang, a value of the maximum read length minus 1 should be provided."
 
 fi
