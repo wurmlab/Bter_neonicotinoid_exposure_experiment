1697f95d36398dfe4565d1098acb5e89ea2debbd
Joe Colgan <t.colgan@qmul.ac.uk>
2018-05-03 16:51:31 +0100

    Updated for arguments taken from the command line

--- quality_assessment/make_star_index.sh
+++ quality_assessment/make_star_index.sh
@@ -1,9 +1,22 @@
 #!/bin/env bash
 
+## Take inputs from the command line:
+input_fasta=$1
+input_gtf=$2
+overhang=$3
+
+## Check arguments are provided:
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Usage: ./make_STAR.sh input.fasta input.gtf overhang"
+    echo "For overhang, a value of the maximum read length minus 1 should be provided."
+
+fi
+
 ## Create STAR indices
 ../../../../../bin/STAR --runThreadN 10 \
                --runMode genomeGenerate \
                --genomeDir . \
-               --genomeFastaFiles GCF_000214255.1_Bter_1.0_genomic.fna \
-               --sjdbGTFfile GCF_000214255.1_Bter_1.0_genomic.gtf \
-               --sjdbOverhang 74
+               --genomeFastaFiles "$input_fasta" \
+               --sjdbGTFfile "$input_gtf" \
+               --sjdbOverhang "$overhang" 
